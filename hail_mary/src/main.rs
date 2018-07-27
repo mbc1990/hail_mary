d135cfd02468bf6729ff486833c96a968340547a
Malcolm <malcolm.balch@gonift.com>
2018-07-27 11:47:49 -0400

    UDP server, test script

--- hail_mary/src/main.rs
+++ hail_mary/src/main.rs
@@ -1,6 +1,30 @@
 use std::env;
+use std::str;
+use std::thread;
+use std::net::UdpSocket;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+    let socket = match UdpSocket::bind("0.0.0.0:5514") {
+        Ok(s) => s,
+        Err(e) => panic!("couldn't bind socket: {}", e)
+    };
+
+    let mut buf = [0; 2048];
+    println!("About to start udp server");
+    loop {
+        match socket.recv_from(&mut buf) {
+            Ok((amt, src)) => {
+                thread::spawn(move || {
+                    println!("amt: {}", amt);
+                    println!("src: {}", src);
+                    println!("{}", str::from_utf8(&buf).unwrap_or(""));
+                });
+            },
+            Err(e) => {
+                println!("couldn't recieve a datagram: {}", e);
+            }
+        }
+    }
 }
---
+++ hail_mary/test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "help" > /dev/udp/127.0.0.1/5514
